eae7879ddcf0e59b98391137bd270fe9733108fe
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-06-06 01:05:24 +0200

    Initial commit

---
+++ src/main/java/variablesAndFlowControl/IncrementDecrement.java
@@ -0,0 +1,21 @@
+package variablesAndFlowControl;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int homeGrade = 2;
+        System.out.println("The value of homeGrade variable is: " + homeGrade);
+        System.out.println("Incrementing homeGrade using syntax: homeGrade++");
+        homeGrade++;
+        System.out.println("The value of homeGrade after incrementation (homeGrade++) is: " + homeGrade);
+
+        System.out.println("##############################################################################");
+
+        int newHomeGrade = 2;
+        System.out.println("The value of newHomeGrade variable is: " + newHomeGrade);
+        System.out.println("Decrementing newHomeGrade using syntax: newHomeGrade--");
+        newHomeGrade--;
+        System.out.println("The value of newHomeGrade after decrementation (newHomeGrade--) is: " + newHomeGrade);
+
+    }
+}
