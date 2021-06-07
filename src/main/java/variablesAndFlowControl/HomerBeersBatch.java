0e1eb2b048573bb0814f6c223418f12b6a71810f
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-06-07 13:53:07 +0200

    if statement, ternary operator and logical operators

---
+++ src/main/java/variablesAndFlowControl/HomerBeersBatch.java
@@ -0,0 +1,17 @@
+package variablesAndFlowControl;
+
+public class HomerBeersBatch {
+    public static void main(String[] args) {
+
+        int batchOfBeer1 = 5;
+        int batchOfBeer2 = 5;
+
+        if (++batchOfBeer2 > batchOfBeer1 || batchOfBeer1++ == batchOfBeer2++){
+            batchOfBeer1++;
+        }
+
+        batchOfBeer1 = (batchOfBeer1++ > batchOfBeer2 && ++batchOfBeer2 < batchOfBeer1++ ? ++batchOfBeer1 : ++batchOfBeer2);
+
+        System.out.println("Homer will have: " + batchOfBeer1-- + " and " + batchOfBeer2++ +" beers.");
+    }
+}
