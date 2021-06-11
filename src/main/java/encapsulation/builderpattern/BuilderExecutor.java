0c0cec1285a1da109ec7d1044975a5878d399e41
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-06-11 16:17:02 +0200

    Using Builder pattern.

---
+++ src/main/java/encapsulation/builderpattern/BuilderExecutor.java
@@ -0,0 +1,14 @@
+package encapsulation.builderpattern;
+
+public class BuilderExecutor {
+
+    public static void main(String[] args) {
+
+        BigBoss bigBoss = new BigBoss.Builder("Military uniform", "MK22")
+                .withExtraWeapons("AK-47", "CQC knife")
+                .withMedicineItems(new String[] {"First Aid Kit"})
+                .build();
+
+        System.out.printf("%s , %s , %s , %s , %s", bigBoss.getStealthCloth(), bigBoss.getPrimaryWeapon(), bigBoss.getSecondaryWeapon(), bigBoss.getMeleeWeapon(), bigBoss.getMedicineItems()[0]);
+    }
+}
