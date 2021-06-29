f3dcbfba3c3238fc588a61b3af5b546e7db0c0a5
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-06-29 14:42:04 +0200

    Chewbacca Default Attack Challenge

---
+++ src/main/java/chapter7_interfaces/Anakin.java
@@ -0,0 +1,28 @@
+package chapter7_interfaces;
+
+public class Anakin implements Jedi, Sith{
+
+    public static void main(String[] args) {
+        new Anakin().useSaber();
+
+    }
+
+    @Override
+    public void useSaber() {
+        System.out.println("Anakin's Saber slash");
+        Jedi.super.useSaber();
+        Sith.super.useSaber();
+    }
+}
+
+interface Jedi{
+    default void useSaber(){
+        System.out.println("Jedi's Saber slash");
+    }
+}
+
+interface Sith{
+    default void useSaber(){
+        System.out.println("Sith's Saber slash");
+    }
+}
